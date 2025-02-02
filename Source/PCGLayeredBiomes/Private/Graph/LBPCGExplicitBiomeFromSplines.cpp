a8de134bdb4bf5342432437a1e85fb6951f4babb
Jon Langford <JonLangfordUK@users.noreply.github.com>
2025-02-02 21:18:35 +0000

    UE5.5 Support (#1)
    # Description
    Rename all classes with a prefix to avoid collisions with other vanilla class names. I also added a config to allow the editor to upgrade existing instances of the old classes to the new names to help avoid the invalidation of existing work.
    
    # Tested
    - Built for Win64 Dev Editor
    - Built for Win64 Dev Game
    - Built for Win64 Dev Server
    - Tested in Win64 Dev Editor
    - Tested in Win64 Dev Game
    - Built for Win64 Dev Game
    - Built for Win64 Dev Server
    - Built for Win64 Shipping Editor
    - Built for Win64 Shipping Game
    - Built for Win64 Shipping Server
    - Tested in Win64 Shipping Editor
    - Tested in Win64 Shipping Game
    - Built for Win64 Shipping Game
    - Built for Win64 Shipping Server

---
+++ Source/PCGLayeredBiomes/Private/Graph/LBPCGExplicitBiomeFromSplines.cpp
@@ -0,0 +1,174 @@
+﻿/*
+ * Copyright (c) 2024 LazyCatsDev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+
+#include "Graph/LBPCGExplicitBiomeFromSplines.h"
+
+#include "EngineUtils.h"
+#include "LBExplicitBiomeActor.h"
+#include "PCGComponent.h"
+#include "PCGModule.h"
+#include "Data/PCGSpatialData.h"
+#include "Elements/Metadata/PCGMetadataElementCommon.h"
+#include "Helpers/PCGDynamicTrackingHelpers.h"
+#include "Helpers/PCGHelpers.h"
+
+#define LOCTEXT_NAMESPACE "PCGExplicitBiomeFromSplines"
+
+ULBPCGExplicitBiomeFromSplines::ULBPCGExplicitBiomeFromSplines()
+{
+	bDisplayModeSettings = false;
+	Mode = EPCGGetDataFromActorMode::ParseActorComponents;
+}
+
+FPCGElementPtr ULBPCGExplicitBiomeFromSplines::CreateElement() const
+{
+	return MakeShared<FLBPCGExplicitBiomeFromSplines>();
+}
+
+TArray<FPCGPinProperties> ULBPCGExplicitBiomeFromSplines::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(PCGPinConstants::DefaultOutputLabel, EPCGDataType::Spline);
+
+	return PinProperties;
+}
+
+FPCGContext* FLBPCGExplicitBiomeFromSplines::Initialize(const FPCGDataCollection& InputData, TWeakObjectPtr<UPCGComponent> SourceComponent, const UPCGNode* Node)
+{
+	FPCGDataFromActorContext* Context = new FPCGDataFromActorContext();
+	Context->InputData = InputData;
+	Context->SourceComponent = SourceComponent;
+	Context->Node = Node;
+
+	return Context;
+}
+
+bool FLBPCGExplicitBiomeFromSplines::ExecuteInternal(FPCGContext* InContext) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGDataFromActorElement::Execute);
+
+	check(InContext);
+	FPCGDataFromActorContext* Context = static_cast<FPCGDataFromActorContext*>(InContext);
+
+	const UPCGDataFromActorSettings* Settings = Context->GetInputSettings<UPCGDataFromActorSettings>();
+	check(Settings);
+
+	if (!Context->bPerformedQuery)
+	{
+		TFunction<bool(const AActor*)> BoundsCheck = [](const AActor*) -> bool { return true; };
+		const UPCGComponent* PCGComponent = Context->SourceComponent.IsValid() ? Context->SourceComponent.Get() : nullptr;
+		const AActor* Self = PCGComponent ? PCGComponent->GetOwner() : nullptr;
+		if (Self && Settings->ActorSelector.bMustOverlapSelf)
+		{
+			// Capture ActorBounds by value because it goes out of scope
+			const FBox ActorBounds = PCGHelpers::GetActorBounds(Self);
+			BoundsCheck = [ActorBounds, PCGComponent](const AActor* OtherActor) -> bool
+			{
+				const FBox OtherActorBounds = OtherActor ? PCGHelpers::GetGridBounds(OtherActor, PCGComponent) : FBox(EForceInit::ForceInit);
+				return ActorBounds.Intersect(OtherActorBounds);
+			};
+		}
+
+		Context->FoundActors.Reset();
+		
+		const auto* World = Context->SourceComponent->GetWorld();
+		for (TActorIterator<AActor> It(World, ALBExplicitBiomeActor::StaticClass()); It; ++It)
+		{
+			AActor* Actor = *It;
+			if (BoundsCheck(Actor))
+			{
+				Context->FoundActors.Add(Actor);
+			}
+		}
+		
+		Context->bPerformedQuery = true;
+
+		if (Context->FoundActors.IsEmpty())
+		{
+			return true;
+		}
+	}
+
+	if (Context->bPerformedQuery)
+	{
+		ProcessActors(Context, Settings, Context->FoundActors);
+
+		// Register dynamic tracking
+#if WITH_EDITOR
+		FPCGDynamicTrackingHelper::AddSingleDynamicTrackingKey(Context, FPCGSelectionKey(ALBExplicitBiomeActor::StaticClass()), /*bIsCulled=*/false);
+#endif // WITH_EDITOR
+	}
+
+	return true;
+}
+
+void FLBPCGExplicitBiomeFromSplines::ProcessActors(FPCGContext* Context, const UPCGDataFromActorSettings* Settings, const TArray<AActor*>& FoundActors) const
+{
+	for (AActor* Actor : FoundActors)
+	{
+		ProcessActor(Context, Settings, Actor);
+	}
+}
+
+void FLBPCGExplicitBiomeFromSplines::ProcessActor(FPCGContext* Context, const UPCGDataFromActorSettings* Settings, AActor* FoundActor) const
+{
+	check(Context);
+	check(Settings);
+
+	if (!FoundActor || !IsValid(FoundActor))
+	{
+		return;
+	}
+
+	TArray<FPCGTaggedData>& Outputs = Context->OutputData.TaggedData;
+
+	const auto Collection = UPCGComponent::CreateActorPCGDataCollection(FoundActor, Context->SourceComponent.Get(), Settings->GetDataFilter(), true);
+
+	if (const auto* Actor = Cast<ALBExplicitBiomeActor>(FoundActor))
+	{
+		for (auto& Item: Collection.TaggedData)
+		{
+			if (const UPCGSpatialData* SpatialData = Cast<UPCGSpatialData>(Item.Data))
+			{
+				auto* Attribute = ClearOrCreateAttribute(SpatialData->Metadata, "Biome", Actor->Biome);
+				if (!Attribute)
+				{
+					PCGE_LOG(Error, GraphAndLog, LOCTEXT("ErrorCreatingAttribute", "Error while creating attribute 'Biome'"));
+				}
+			}
+		}
+	}
+
+	Outputs += Collection.TaggedData;
+}
+
+FPCGMetadataAttributeBase* FLBPCGExplicitBiomeFromSplines::ClearOrCreateAttribute(UPCGMetadata* Metadata, const FName OutputAttributeName, const FName Value)
+{
+	check(Metadata);
+
+	return PCGMetadataElementCommon::ClearOrCreateAttribute(Metadata, OutputAttributeName, Value);
+}
+
+#undef LOCTEXT_NAMESPACE
+
---
+++ Source/PCGLayeredBiomes/Public/Biomes/LBBiomesSettings.h
@@ -0,0 +1,132 @@
+﻿/*
+ * Copyright (c) 2024 LazyCatsDev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGCrc.h"
+#include "UObject/Object.h"
+#include "Engine/DataAsset.h"
+#include "LBBiomesSettings.generated.h"
+
+class ULBPCGBiomesBaseFilter;
+class UPCGMetadata;
+struct FPCGPoint;
+struct FBiomeFilter;
+class UPCGGraph;
+class ULBBaseBiomeLayer;
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct PCGLAYEREDBIOMES_API FLBBiomeSettings
+{
+	GENERATED_BODY()
+
+	/**
+	 * Disabled biomes doesn't generate any content
+	 */
+	UPROPERTY(EditDefaultsOnly, AdvancedDisplay, Category=Biomes)
+	bool Enabled = true;
+
+	UPROPERTY(EditDefaultsOnly, AdvancedDisplay, Category=Biomes, meta=(InlineEditConditionToggle))
+	bool Debug = false;
+	/**
+	 * Draw mask of the biome with specified color
+	 */
+	UPROPERTY(EditDefaultsOnly, AdvancedDisplay, Category=Biomes, AdvancedDisplay, meta=(EditCondition=Debug))
+	FLinearColor DebugColor = FLinearColor::Transparent;
+
+	/**
+	 * Sets priority of the biome. If landscape point can passed filters for several biomes,
+	 * biome with lower priority take precedence.  
+	 */
+	UPROPERTY(EditDefaultsOnly, Category=Biomes)
+	int32 Priority = 5000;
+	
+	/**
+	 * All filters should pass (return `true`) for a point to include the point to the biome.
+	 */
+	UPROPERTY(EditDefaultsOnly, Instanced, Category=Biomes)
+	TArray<TObjectPtr<ULBPCGBiomesBaseFilter>> Filters;
+
+	/**
+	 * Content generation layers.
+	 * Executed in specified order.
+	 */
+	UPROPERTY(EditDefaultsOnly, Instanced, Category=Biomes, meta=(TitleProperty = "SpawnSet"))
+	TArray<TObjectPtr<ULBBaseBiomeLayer>> Layers;
+};
+
+USTRUCT()
+struct PCGLAYEREDBIOMES_API FLBBiomeSettings_Named : public FLBBiomeSettings
+{
+	GENERATED_BODY()
+
+	FLBBiomeSettings_Named() = default;
+	FLBBiomeSettings_Named(FName InName, FLBBiomeSettings InSettings);
+	
+	UPROPERTY(EditDefaultsOnly, Category=Biomes)
+	FName Name;
+};
+
+UCLASS(BlueprintType, Transient, ClassGroup=(Biomes))
+class PCGLAYEREDBIOMES_API ULBBiomesData : public UObject
+{
+	GENERATED_BODY()
+
+	friend class ULBBiomesSettings;
+	
+public:
+	UFUNCTION(BlueprintCallable, Category=Biomes)
+	bool DetectBiome(const FPCGPoint& Point, const UPCGMetadata* Metadata, FName& OutBiome, int& OutPriority) const;
+
+protected:
+	UPROPERTY(Transient)
+	TArray<FLBBiomeSettings_Named> Biomes;
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, ClassGroup=(Biomes))
+class PCGLAYEREDBIOMES_API ULBBiomesSettings : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	FPCGCrc ComputeCrc();
+
+	UFUNCTION(BlueprintCallable, Category=Biomes)
+	ULBBiomesData* Prepare() const; 
+
+	const FLBBiomeSettings* FindSettings(FName Name) const;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Biomes)
+	TMap<FName, FLBBiomeSettings> Biomes;
+};
